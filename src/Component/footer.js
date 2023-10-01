bb19c73787d089c78f20b48f22d97916adbebc1e
Peter Wang <69640977+pwang1092@users.noreply.github.com>
2023-09-30 22:13:44 -0700

    commit

--- src/Component/footer.js
+++ src/Component/footer.js
@@ -8,7 +8,7 @@ import linkedin from '../images/linkedin.png';
 const Footer = () => {
     return (
         <footer className='footer'>
-            <div className="email">©Copyright 2023 does not exist</div>
+            <div className="email">Email me at: plwang@usc.edu</div>
 
             <div className="imgs">
                 <a href="https://www.linkedin.com/in/usc-peter-wang/" target="_blank" rel="noreferrer">
