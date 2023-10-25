31ba7fd5fa87f1a7d418317d879244b4ec343fd2
Peter Wang <69640977+pwang1092@users.noreply.github.com>
2023-10-25 16:30:20 -0700

    Update intro.js

--- src/Component/intro.js
+++ src/Component/intro.js
@@ -9,7 +9,7 @@ const Intro = () => {
         <section id="intro">
             <div className="introContent">
                 <span className="hello">Hello, </span>
-                <span className="introText">I'm <span className="introName">Peter Wang</span><br />Computer Science student</span>
+                <span className="introText">I'm <span className="introName">Peter Wang</span><br />Computer Science Student and Chess Fanatic</span>
                 <p className="introPara"> I'm a student at the University of Southern California. In my free time, I enjoy practicing piano, hiking, and playing board games, especially chess! If you play, feel free to challenge me on Chess.com at plw_usc! 
                 </p>
 
