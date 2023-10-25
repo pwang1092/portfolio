b85ee4561481e150f5439106138fc37ebed7cb0b
Peter Wang <69640977+pwang1092@users.noreply.github.com>
2023-10-25 16:18:47 -0700

    Update intro.js

--- src/Component/intro.js
+++ src/Component/intro.js
@@ -10,8 +10,7 @@ const Intro = () => {
             <div className="introContent">
                 <span className="hello">Hello, </span>
                 <span className="introText">I'm <span className="introName">Peter Wang</span><br />Computer Science student</span>
-                <p className="introPara"> I'm a student at the University of Southern California. In my free time, I enjoy practicing piano, hiking, and playing board games. 
-                    I especially love playing chess! 
+                <p className="introPara"> I'm a student at the University of Southern California. In my free time, I enjoy practicing piano, hiking, and playing board games, especially love playing chess! 
                 </p>
 
                 {/** button scrolls down to contact form */}
